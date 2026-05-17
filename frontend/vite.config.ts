15a13d71e670d3223942f323991e9cc9700f82c3
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-05-16 21:34:12 -0500

    impl: added vite for frontend and generated frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+// frontend/vite.config.ts
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+    build: {
+        outDir: '../src/main/resources/static',
+        emptyOutDir: true,
+    },
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8080',
+            '/ws': {
+                target: 'ws://localhost:8080',
+                ws: true,
+            }
+        }
+    }
+})
