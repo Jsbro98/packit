ff582445d03111b0168b5c82da0a505b51b43904
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-05-16 20:40:54 -0500

    init: PackIt project setup

---
+++ src/main/java/com/jsbro98/packit/PackitApplication.java
@@ -0,0 +1,13 @@
+package com.jsbro98.packit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsbro98/packit/PackitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsbro98.packit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
