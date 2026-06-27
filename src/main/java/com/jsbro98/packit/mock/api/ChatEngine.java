edeab01c190141a524a2291cb434c592fed27396
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-06-27 17:58:14 -0500

    change: renamed method to reflect adding listeners

--- src/main/java/com/jsbro98/packit/mock/api/ChatEngine.java
+++ src/main/java/com/jsbro98/packit/mock/api/ChatEngine.java
@@ -5,5 +5,7 @@ import com.jsbro98.packit.mock.model.Message;
 public interface ChatEngine {
   void sendMessage(Message message);
 
-  void onMessageReceived(MessageListener message);
+  // listeners used here to compose behavior as needed
+  // ex. add a listener for validation or logging before sending the actual message
+  void registerListener(MessageListener listener);
 }
