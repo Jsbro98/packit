8331608390bbe1e20502a47b8e0ecee69e4aa8cd
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-08-07 22:03:13 -0500

    change: added a TODO

--- src/main/java/com/jsbro98/packit/controller/ChatController.java
+++ src/main/java/com/jsbro98/packit/controller/ChatController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+// TODO: create controller advice for this class
 @Controller
 public class ChatController {
   private static final Logger LOGGER = LoggerFactory.getLogger(ChatController.class);
